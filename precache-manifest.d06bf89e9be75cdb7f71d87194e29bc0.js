eaac42e958a6c6df24034ce807958093f541dc93
ellie25v <yelizarieva.ellie@gmail.com>
2020-03-06 14:51:24 +0200

    Updates

---
+++ precache-manifest.d06bf89e9be75cdb7f71d87194e29bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a91501a62310363890f0cae41c7b13e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "39fa7cbb0bfe4c304904",
+    "url": "/goit-react-hw-01-components/static/css/main.d91419bb.chunk.css"
+  },
+  {
+    "revision": "a91efe6ca5690fc0ec57",
+    "url": "/goit-react-hw-01-components/static/js/2.3ef8dd99.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.3ef8dd99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39fa7cbb0bfe4c304904",
+    "url": "/goit-react-hw-01-components/static/js/main.b443da3f.chunk.js"
+  },
+  {
+    "revision": "fcdcaaac4856f123fd20",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.6a0abf10.js"
+  }
+]);
